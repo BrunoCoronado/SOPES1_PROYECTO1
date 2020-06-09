a0e9eacf1b790b60bdebc4c29cb5ace938256596
Bruno Coronado <bcoronado.morales@gmail.com>
2020-06-08 21:11:52 -0600

    pruebas con modulos de kernel

--- Pruebas/cmake-build-debug/cmake_install.cmake
+++ Pruebas/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/trext/Documentos/Sopes/Pruebas
+# Install script for directory: /home/trext/Documentos/Sopes/SOPES1_PROYECTO1/Pruebas
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/trext/Documentos/Sopes/Pruebas/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/trext/Documentos/Sopes/SOPES1_PROYECTO1/Pruebas/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- modulos_kernel/.tmp_versions/tronco.mod
+++
@@ -1,3 +0,0 @@
-/home/trext/Documentos/Sopes/SOPES1_PROYECTO1/modulos_kernel/tronco.ko
-/home/trext/Documentos/Sopes/SOPES1_PROYECTO1/modulos_kernel/tronco.o
-
